d7ea78f37c4744a2ed36f304e1af286d0eea1ba9
oobii65 <oivind@berggraf.com>
2014-11-16 20:19:56 +0100

    Update2

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,13 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##
+## Use the R lexical scope and the <<- operator to
+## to make a variabel in the "parent" environment that 
+## can be used to hold a cached version of the inverse 
+## of a matrix. 
+##
+##
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -19,7 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+##
+## Returns the inverse of a matrix that is declared using the 
+## makeCacheMatrix(...) function.
+## When an inverse is computed it is cached in the "parent" environment 
+## using the <<- operator.
+## If a cached version exists, this will be returned without performing
+## any computations.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 
